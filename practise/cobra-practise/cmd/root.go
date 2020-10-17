327b658c41ff205eb3b0088e60b2fb63f8b9ea54
caoyuan <cao.yingjunz@gmail.com>
2020-10-17 11:19:24 +0800

    Add cobra practise
    Change-Id: I225d4985ea40f15a9418351f880150a21a512059

---
+++ practise/cobra-practise/cmd/root.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"golang-learning/practise/cobra-practise/imp"
+)
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "test of cobra",
+	Short: "A short demo cobra",
+	Long: `Demo is a test appcation for print things.`,
+
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		imp.Show("caoyingjun", 18)
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+	viper.SetConfigName(".demo")
+	viper.AddConfigPath("$HOME")
+	viper.AutomaticEnv()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
+	RootCmd.Flags().BoolP("toggle", "t", false, "help message for toggle")
+}
---
+++ practise/cobra-practise/main.go
@@ -0,0 +1,12 @@
+package main
+
+// refer to https://studygolang.com/articles/7588
+// https://www.cnblogs.com/sparkdev/p/10856077.html
+
+import (
+	"golang-learning/practise/cobra-practise/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
