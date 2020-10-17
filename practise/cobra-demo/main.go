7b48c18e92ba89ec531b71188b0d6c8f940ff36c
caoyuan <cao.yingjunz@gmail.com>
2020-10-17 11:19:24 +0800

    Add cobra practise
    Change-Id: I225d4985ea40f15a9418351f880150a21a512059

--- practise/cobra-demo/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"golang-learning/practise/cobra-demo/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- practise/http-practise.go
+++ practise/http-practise.go
@@ -3,42 +3,42 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
-	"time"
 )
 
-func init() {
-	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
-}
+const (
+	Url = "http://www.baidu.com"
+)
 
 func main() {
 	client := &http.Client{}
-
-	t1 := time.Now()
 	// 发送一个请求
-	req, err := http.NewRequest("POST", "http://163.com/", strings.NewReader("key=value"))
-
+	req, err := http.NewRequest("POST", Url, strings.NewReader("key=value"))
+	// 必须加 close
+	defer req.Body.Close()
 	if err != nil {
-		log.Print("Get failed: err:", err)
+		fmt.Printf("err: %v", err)
 		return
 	}
 
-	defer req.Body.Close()
+	// 增加header 或者 cookes
+	req.Header.Add("Content-Type", "application/json")
 
-	req.Header.Add("User-Agent", "myClient")
+    // 增加 cookes
+    cookie1 := &http.Cookie{Name: "name", Value: "caoyingjun"}
+	req.AddCookie(cookie1)
 
 	resp, err := client.Do(req)
-
 	if err != nil {
-		log.Print("Read body failed: ", err)
+		fmt.Printf("%v", err)
 		return
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
 	fmt.Print(string(data))
-
-	fmt.Println(time.Now().Sub(t1))
-
 }
