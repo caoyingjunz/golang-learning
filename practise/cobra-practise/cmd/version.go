22520d47b292de01f0f230a592d42ead0980c7a3
caoyingjun <cao.yingjunz@gmail.com>
2020-10-19 09:06:25 +0800

    Add cobra practise [part3]
    Change-Id: I920d030396142db056f60e20556e5b098e7e47fd

--- practise/cobra-practise/cmd/version.go
+++ practise/cobra-practise/cmd/version.go
@@ -12,17 +12,22 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A short versionCmd demo",
+	Short: "Show the demo version information",
 	Long:  `A Long versionCmd demo`,
 	// PersistentPreRun(global)： 子命令会继承 rootCmd
 	// PreRun(local): 子命令不会继承 rootCmd
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Run versionCmd PersistentPreRun with args: %v\n", args)
+		//fmt.Printf("Run versionCmd PersistentPreRun with args: %v\n", args)
+		fmt.Printf("")
 	},
-	PreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("run versionCmd PreRun with args: %v\n", args)
-	},
+	//PreRun: func(cmd *cobra.Command, args []string) {
+	//	fmt.Printf("run versionCmd PreRun with args: %v\n", args)
+	//},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Exec versionCmd")
+		fmt.Println("Cobra-demo version is v1.0.0")
+	},
+	// 定义自己的 PersistentPostRun 否则会运行 rootCmd 的 PersistentPostRun
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("")
 	},
 }
