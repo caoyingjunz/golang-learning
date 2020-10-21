c0a06c9ec08039417d8606acf8672b10173e3613
caoyingjun <cao.yingjunz@gmail.com>
2020-10-21 23:43:25 +0800

    The defer should be after the error
    Change-Id: I1f04b39bcaa7899c7110a8dc6e51ecf9ea8361b2

--- practise/http-practise.go
+++ practise/http-practise.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const (
@@ -12,15 +14,21 @@ const (
 )
 
 func main() {
-	client := &http.Client{}
+	client := &http.Client{
+		// TODO: 可以在此处设置超时或者其他
+		// 1. 设置超时
+		//Timeout: time.Second,
+	}
 	// 发送一个 POST 请求
 	req, err := http.NewRequest("POST", Url, strings.NewReader("key=value"))
-	// 必须加 close 去关闭 连接
-	defer req.Body.Close()
 	if err != nil {
-		fmt.Printf("err: %v", err)
+		fmt.Printf("NewRequest error: %v", err)
 		return
 	}
+
+	// 2. 除了 timeout设置，也可以通过 context
+	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	req.WithContext(ctx)
 
 	// 增加header 或者 cookes (可选)
 	req.Header.Add("Content-Type", "application/json")
@@ -31,9 +39,13 @@ func main() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("%v", err)
+		fmt.Printf("response error: %v", err)
 		return
 	}
+
+	// 必须加 close 去关闭 连接
+	// 需要在 err 处理后面，有的时候错误返回的时候，resp 为 nil
+	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
