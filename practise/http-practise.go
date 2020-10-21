9ca5d87630a4b4bd5abd6713aec8237952cebb0c
caoyingjun <cao.yingjunz@gmail.com>
2020-10-21 23:43:25 +0800

    The defer should be after the error
    Change-Id: I1f04b39bcaa7899c7110a8dc6e51ecf9ea8361b2

--- practise/http-practise.go
+++ practise/http-practise.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
 )
+
+// refer to https://blog.csdn.net/cheyo809775692/article/details/100924529
 
 const (
 	Url = "http://www.baidu.com"
 )
 
 func main() {
-	client := &http.Client{}
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse("http://127.0.0.1:1080")
+	}
+
+	noRedirect := func(req *http.Request, via []*http.Request) error {
+		return errors.New("no Redirect")
+	}
+
+	transport := &http.Transport{
+		Proxy:                 proxy,
+		IdleConnTimeout:       time.Second * 2048,
+		ResponseHeaderTimeout: time.Second * 5,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+	}
+
+	client := &http.Client{
+		// TODO: 可以在此处设置超时或者其他
+		Transport:     transport,
+		CheckRedirect: noRedirect,
+	}
 	// 发送一个 POST 请求
 	req, err := http.NewRequest("POST", Url, strings.NewReader("key=value"))
-	// 必须加 close 去关闭 连接
-	defer req.Body.Close()
 	if err != nil {
-		fmt.Printf("err: %v", err)
+		fmt.Printf("NewRequest error: %v", err)
 		return
 	}
+
+	// 2. 除了 timeout设置，也可以通过 context
+	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	req.WithContext(ctx)
 
 	// 增加header 或者 cookes (可选)
 	req.Header.Add("Content-Type", "application/json")
@@ -30,10 +61,17 @@ func main() {
 	req.AddCookie(cookie1)
 
 	resp, err := client.Do(req)
+	// 通过判断resp来准备是否close
+	//if resp != nil {
+	//	defer resp.Body.Close()
+	//}
 	if err != nil {
-		fmt.Printf("%v", err)
+		fmt.Printf("response error: %v", err)
 		return
 	}
+	// 必须加 close 去关闭 连接
+	// 需要在 err 处理后面，有的时候错误返回的时候，resp 为 nil
+	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
