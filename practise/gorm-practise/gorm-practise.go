98af4da36f7ebe9fc225280af1a61a96df4a593d
caoyingjun <cao.yingjunz@gmail.com>
2020-10-22 22:50:57 +0800

    Add gorm docs and gorm-practise
    Change-Id: Id544de9a68cf7946d61481237b0ea4da24045317

---
+++ practise/gorm-practise/gorm-practise.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang-learning/practise/gorm-practise/db"
+)
+
+// refer to http://gorm.book.jasperxu.com
+
+var (
+	id   uint = 123456
+	name      = "caoyingjun"
+)
+
+func main() {
+
+	u := db.NewUserDB()
+
+	//  创建 user
+	//if err := u.CreateUser(id, name); err != nil {
+	//	log.Println("CreateUser", err.Error())
+	//	return
+	//}
+
+	user, err := u.GetUser(name)
+	if err != nil {
+		log.Println("GetUser", err.Error())
+		return
+	}
+	fmt.Println(user.Name, user.Age)
+
+	if err := u.UpdateUser(name); err != nil {
+		log.Println("UpdateUser", err.Error())
+		return
+	}
+
+}
