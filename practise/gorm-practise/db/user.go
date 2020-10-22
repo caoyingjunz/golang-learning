538ea93f5a56622e2ffb64271e0b47f88e3d52e4
caoyingjun <cao.yingjunz@gmail.com>
2020-10-22 22:50:57 +0800

    Add gorm docs
    Change-Id: Id544de9a68cf7946d61481237b0ea4da24045317

---
+++ practise/gorm-practise/db/user.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"time"
+
+	"golang-learning/practise/gorm-practise/models"
+)
+
+type UserInterface interface {
+	GetUser(name string) (user *models.User, err error)
+	CreateUser(id uint, name string) (err error)
+	UpdateUser(id string) (err error)
+	DeleleUser(id string) (err error)
+}
+
+func NewUserDB() UserInterface {
+	return &UserDB{}
+}
+
+type UserDB struct{}
+
+func (u *UserDB) GetUser(name string) (user *models.User, err error) {
+	if err = DB.Where("name = ?", "caoyingjun").First(user).Error; err != nil {
+		return
+	}
+	return
+}
+
+func (u *UserDB) CreateUser(id uint, name string) (err error) {
+	user := models.User{
+		ID:        id,
+		Name:      name,
+		Age:       "18",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	if err = DB.Create(&user).Error; err != nil {
+		return
+	}
+	return
+}
+
+func (u *UserDB) UpdateUser(id string) (err error) {
+	if err = DB.Where("name = %s", "caoyingjun").Update(&models.User{UpdatedAt: time.Now()}).Error; err != nil {
+		return
+	}
+	return
+}
+
+func (u *UserDB) DeleleUser(id string) (err error) {
+	if err = DB.Where("name = %s", "caoyingjun").Delete(&models.User{}).Error; err != nil {
+		return
+	}
+	return
+}
---
+++ practise/gorm-practise/models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint   `gorm:"column:id;primary_key;not null" json:"id"`
+	Name      string `gorm:"column:name;type:varchar(16);not null" json:"name"`
+	Age       string `gorm:"column:age;type:varchar(16);not null" json:"age"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
