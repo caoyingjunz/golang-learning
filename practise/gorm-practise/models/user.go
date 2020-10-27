6c23cf83bfe391840f0377e53f827b1c5bedb956
caoyingjun <cao.yingjunz@gmail.com>
2020-10-27 08:39:44 +0800

    Add raw and AUTO_INCREMENT for gorm practise
    Change-Id: Icf55d2d6291d540dd09647483d58a4dcd285bca6

--- practise/gorm-practise/models/user.go
+++ practise/gorm-practise/models/user.go
@@ -2,10 +2,17 @@ package models
 
 import "time"
 
+//CREATE TABLE `user` (
+//	`id` INT NOT NULL AUTO_INCREMENT,
+//	`name` VARCHAR(16) NOT NULL,
+//	`age` TINYINT NOT NULL,
+//	PRIMARY KEY (`id`)
+//) ENGINE=InnoDB CHARSET=utf8 AUTO_INCREMENT=20110000;
+
 type User struct {
-	ID        uint   `gorm:"column:id;primary_key;not null" json:"id"`
-	Name      string `gorm:"column:name;type:varchar(128);not null" json:"name"`
-	Age       int64  `gorm:"column:age;type:varchar(16);not null" json:"age"`
+	ID        int64  `gorm:"column:id;primary_key;AUTO_INCREMENT;not null" json:"id"`
+	Name      string `gorm:"column:name;type:varchar(128);index:idx_name;not null" json:"name"` // index 需要在看看
+	Age       int    `gorm:"column:age;not null" json:"age"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
